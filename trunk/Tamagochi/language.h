fcb1a44c0e736c6b50004f1ff28e873e862c33db
anis_1981@mail.ru <anis_1981@mail.ru@f75f4211-f83f-0410-8a70-e9417351f98b>
2007-12-05 05:06:28 +0000

    Перезалил Tamagochi

---
+++ trunk/Tamagochi/language.h
@@ -0,0 +1,220 @@
+#ifndef _LANGUAGE_H_
+#define _LANGUAGE_H_
+
+//Оставляем только один дефайн иначе будет кака
+#define LANG_RU
+//#define LANG_EN
+
+#ifdef LANG_RU
+
+//===============================
+
+#define LG_GFGHELLO "Приветствие"
+#define LG_GFGYES "Да"
+#define LG_GFGNO "Нет"
+
+#define LG_GFGSETSCREEN "Настройки смайла"
+#define LG_GFGUPDATE "Обновление, 1/10 сек"
+#define LG_GFGVIEW "Показовать на"
+#define LG_GFGLOCK "Блокирован"
+#define LG_GFGUNLOCK "Разблокирован"
+#define LG_GFGBOTH "На обоих"
+#define LG_GFGPOSITION "Координаты на экране"
+#define LG_GFGMINSIZE "Min Size (%)"
+#define LG_GFGMAXSIZE "Max Size (%)"
+#define LG_GFGOPACITY "Opacity (%)"
+
+#define LG_GFGSETPATH "Настройка путей"
+#define LG_GFGPATHPET "Путь к питомцу"
+#define LG_GFGPATHROOM "Путь к комнате"
+#define LG_GFGPATHIMG "Путь к картинкам"
+#define LG_GFGPATHSOUND "Путь к звукам"
+
+#define LG_GFGSETPET "Настройки развития"
+#define LG_GFGSPEED "Скорость"
+#define LG_GFGFAST "Быстро"
+#define LG_GFGMEDIUM "Средне"
+#define LG_GFGSLOW "Медленно"
+
+#define LG_GFGSETALARM "Оповещение"
+#define LG_GFGVOLUME "Громкость звуков"
+
+#define LG_GFGSETKEY "Вызов меню"
+#define LG_GFGMNUENA "Тип вызова"
+#define LG_GFGKEY "Клавиша"
+//===============================
+#define LG_COOL "Клева!"
+#define LG_IAMSLEEP "Не мешай спать!"
+#define LG_PAIN "Больна же!"
+#define LG_SHREW "Злюка!"
+#define LG_IAMDIE "Я уже умер!Поздняк метаться!"
+
+#define LG_MSGSLEEP "Нехочу спать!"
+#define LG_MSGNOTSLEEP "У блин разбудили!"
+
+#define LG_ALREADY_STARTED "Уже запущена!"
+
+#define LG_MENU    "Меню"
+#define LG_GAMEPLAY "Игра"
+#define LG_VACCINATE  "Лечение"
+#define LG_DISCIPLINE  "Дисциплина"
+#define LG_SLEEP  "Спать"
+#define LG_NOTSLEEP  "Проснись"
+#define LG_STATUS  "Статус"
+#define LG_EDCFG   "Настройки"
+#define LG_ABOUT   "Об эльфе"
+
+#define LG_AGE "Возраст"
+#define LG_HEALTH  "Здоровье"
+#define LG_HUNGER  "Голод"
+#define LG_HAPPINESS  "Счастье"
+#define LG_DIRTINESS  "Чистота"
+#define LG_BOREDOM  "Скука"
+#define LG_BEHAVIOUR  "Мораль"
+    
+#define LG_MSGAGE "Ура! Днюха!"
+#define LG_MSGHEALTH  "Мало жизни! Сдохну скоро!"
+#define LG_MSGHUNGER  "Жрать давай! А то скоро загнусь!"
+#define LG_MSGHAPPINESS  "Жизнь гавно!"
+#define LG_MSGDIRTINESS  "Живу как свинья!"
+#define LG_MSGBOREDOM  "Скука смертная!"
+
+//-------------
+#define LG_SELECT  "Выбор"
+#define LG_CLOSE   "Закрыть"
+#define LG_BACK    "Назад"
+//--------------
+
+//================================
+
+#define LG_LOADING "Tamagochi загружен!"
+#define LG_UPDSETTING "Настройки Tamagochi применены!"
+
+//++++++++++++++++++++++++++++++++++++++++
+
+static const int mess_mask=17-1;
+static const char * const SpeakMessage[17]=
+{
+  "Аффтар жжет, пеши исчо!",
+  "Ф бабруйск животнайе!",
+  "Аццкий сотона!",
+  "Выпей йаду!",
+  "Отъебитесь бляди, йа выучил албанский!",
+  "Блиа!! Ниибаццо жжош!!!",
+  "Бу-га-га!",
+  "Жжошь как агнимьот!!!",
+  "Напирисдачунах!",
+  "Ктулху зохавал твой моск!",
+  "ЙА КРИВЕТКО!",
+  "Нубляващще?!",
+  "Убей сибя апстену!",
+  "Учи албанский!",
+  "Фублянах!",
+  "Шозонах?!",
+  "Яибу!"
+};
+#endif
+
+
+
+#ifdef LANG_EN
+
+#define LG_GFGHELLO "Hello message"
+#define LG_GFGYES "Yes"
+#define LG_GFGNO "No"
+
+#define LG_GFGSETSCREEN "Setting screen"
+#define LG_GFGUPDATE "Refresh period, 1/10 sec"
+#define LG_GFGVIEW "Show In"
+#define LG_GFGLOCK "Lock"
+#define LG_GFGUNLOCK "Unlock"
+#define LG_GFGBOTH "Both"
+#define LG_GFGPOSITION "Position"
+#define LG_GFGMINSIZE "Min Size (%)"
+#define LG_GFGMAXSIZE "Max Size (%)"
+#define LG_GFGOPACITY "Opacity (%)"
+
+#define LG_GFGSETPATH "Setting path"
+#define LG_GFGPATHPET "Path pet"
+#define LG_GFGPATHROOM "Path room"
+#define LG_GFGPATHIMG "Path image"
+#define LG_GFGPATHSOUND "Peth sound"
+
+#define LG_GFGSETPET "Setting Pet"
+#define LG_GFGSPEED "Speed"
+#define LG_GFGFAST "Fast"
+#define LG_GFGMEDIUM "Medium"
+#define LG_GFGSLOW "Slow"
+
+#define LG_GFGSETALARM "Notify"
+#define LG_GFGVOLUME "Volume sound"
+
+#define LG_GFGSETKEY "Activation menu"
+#define LG_GFGMNUENA "Activation style"
+#define LG_GFGKEY "Activation key"
+//===============================
+
+#define LG_COOL "Cool!"
+#define LG_IAMSLEEP "I am sleep!"
+#define LG_PAIN "PAIN!"
+#define LG_SHREW "SHREW!"
+
+#define LG_MSGSLEEP "I am sleep!"
+#define LG_MSGNOTSLEEP "I am wakeup!"
+
+#define LG_ALREADY_STARTED "Already started!"
+
+#define LG_MENU    "Menu"
+#define LG_GAMEPLAY "Game"
+#define LG_VACCINATE  "Vaccinate"
+#define LG_DISCIPLINE  "Discipline"
+#define LG_SLEEP  "Sleep"
+#define LG_NOTSLEEP  "Wakeup"
+#define LG_STATUS  "Status"
+#define LG_EDCFG   "Setting"
+#define LG_ABOUT   "About"
+
+#define LG_AGE "Age"
+#define LG_HEALTH  "Health"
+#define LG_HUNGER  "Hunger"
+#define LG_HAPPINESS  "Happiness"
+#define LG_DIRTINESS  "Cleanness"
+#define LG_BOREDOM  "Boredom"
+#define LG_BEHAVIOUR  "Behavior"
+    
+#define LG_IAMDIE "Death!"
+#define LG_MSGAGE "Birthday!"
+#define LG_MSGHEALTH  "HEALTH!"
+#define LG_MSGHUNGER  "HUNGER!"
+#define LG_MSGHAPPINESS  "Low HAPPINESS!"
+#define LG_MSGDIRTINESS  "DIRTINESS!"
+#define LG_MSGBOREDOM  "BOREDOM!"
+
+//-------------
+#define LG_SELECT  "Select"
+#define LG_CLOSE   "Close"
+#define LG_BACK    "Back"
+//--------------
+
+//================================
+
+#define LG_LOADING "Tamagochi loading!"
+#define LG_UPDSETTING "Setting Tamagochi updated!"
+
+
+
+//++++++++++++++++++++++++++++++++++++++++
+
+static const int mess_mask=2-1;
+static const char * const SpeakMessage[2]=
+{
+  "Fuck you!",
+  "Bitch!"
+};
+
+
+#endif
+
+#endif
+
+
---
+++ trunk/Tamagochi/status.c
@@ -0,0 +1,207 @@
+#include "..\inc\swilib.h"
+#include "conf_loader.h"
+#include "rect_patcher.h"
+#include "..\inc\pnglist.h"
+#include "revision.h"
+#include "Tamagochi.h"
+#include "language.h"
+
+
+int StatusMenu_ID;
+
+extern int M_ICONS[];
+extern TStatusPet StatusPet;
+
+void ViewAge()
+{
+  char s[256];
+  snprintf(s,255,"%s:%d\n%d/%d",LG_AGE,
+            StatusPet.Age,
+            (int)StatusPet.TimeAge,
+            StatusPet.Age*10+100 );
+   ShowMSG(2, (int)s);
+}
+void ViewHealth()
+{
+  char s[256];
+   snprintf(s,255,"%s:%d%%",LG_HEALTH,
+            (int)StatusPet.Health*100/StatusPet.MaxHealth
+            );
+   ShowMSG(2, (int)s);
+}
+void ViewHunger()
+{
+  char s[256];
+  snprintf(s,255,"%s:%d%%",LG_HUNGER,
+            (int)StatusPet.Hunger*100/StatusPet.MaxHunger
+            );
+  ShowMSG(2, (int)s);
+}
+void ViewHappiness()
+{
+  char s[256];
+  snprintf(s,255,"%s:%d%%",LG_HAPPINESS,
+            (int)StatusPet.Happiness*100/StatusPet.MaxHappiness
+            );
+  ShowMSG(2, (int)s);
+}
+void ViewDirtiness()
+{
+  char s[256];
+  snprintf(s,255,"%s:%d%%",LG_DIRTINESS,
+            100-(int)StatusPet.Dirtiness*100/StatusPet.MaxDirtiness
+            );
+  ShowMSG(2, (int)s);
+}
+void ViewBoredom()
+{
+  char s[256];
+  snprintf(s,255,"%s:%d%%",LG_BOREDOM,
+            (int)StatusPet.Boredom*100/StatusPet.MaxBoredom
+            );
+  ShowMSG(2, (int)s);
+}
+void ViewBehaviour()
+{
+  char s[256];
+  snprintf(s,255,"%s:%d%%",LG_BEHAVIOUR,
+            (int)StatusPet.Behaviour*100/StatusPet.MaxBehaviour
+            );
+  ShowMSG(2, (int)s);
+}
+
+
+///-----------------------
+
+static const HEADER_DESC smenuhdr={0,0,131,21,NULL,(int)LG_STATUS,LGP_NULL};
+
+static const int smenusoftkeys[]={0,1,2};
+
+static const char * const smenutexts[7]=
+{
+  LG_AGE,
+  LG_HEALTH,
+  LG_HUNGER,
+  LG_HAPPINESS,
+  LG_DIRTINESS,
+  LG_BOREDOM,
+  LG_BEHAVIOUR
+};
+
+
+
+static const void *smenuprocs[7]=
+{
+  (void *)ViewAge,
+  (void *)ViewHealth,
+  (void *)ViewHunger,
+  (void *)ViewHappiness,
+  (void *)ViewDirtiness,
+  (void *)ViewBoredom,
+  (void *)ViewBehaviour
+ 
+};
+
+static const SOFTKEY_DESC smenu_sk[]=
+{
+  {0x0018,0x0000,(int)LG_SELECT},
+  {0x0001,0x0000,(int)LG_BACK},
+  {0x003D,0x0000,(int)LGP_DOIT_PIC}
+};
+
+static const SOFTKEYSTAB smenu_skt=
+{
+  smenu_sk,0
+};
+
+void smenu_ghook(void *data, int cmd)
+{
+  PNGTOP_DESC *pltop=PNG_TOP();
+  if (cmd==9)
+  {
+    pltop->dyn_pltop=NULL;
+  }
+  if (cmd==0x0A)
+  {
+    DisableIDLETMR();
+  }
+}
+
+static int smenu_keyhook(void *data, GUI_MSG *msg)
+{
+  if ((msg->keys==0x18)||(msg->keys==0x3D))
+  {
+    ((void (*)(void))(smenuprocs[GetCurMenuItem(data)]))();
+  }
+  return(0);
+}
+
+static void smenuitemhandler(void *data, int curitem, void *unk)
+{
+  WSHDR *ws;
+  void *item=AllocMenuItem(data);
+  switch(curitem)
+  {
+  case 0:{
+    ws=AllocMenuWS(data,strlen(smenutexts[curitem])+6);
+    wsprintf(ws,"%t: %d",smenutexts[curitem],StatusPet.Age);
+    SetMenuItemIconArray(data,item,M_ICONS+7);
+    SetMenuItemText(data, item, ws, curitem);
+    break;}
+  case 1:
+    ws=AllocMenuWS(data,strlen(smenutexts[curitem])+5);
+    wsprintf(ws,"%t:%d%%",smenutexts[curitem],(int)StatusPet.Health*100/StatusPet.MaxHealth);
+    SetMenuItemIconArray(data,item,M_ICONS+8);
+    SetMenuItemText(data, item, ws, curitem);
+    break;
+  case 2:
+    ws=AllocMenuWS(data,strlen(smenutexts[curitem])+5);
+    wsprintf(ws,"%t:%d%%",smenutexts[curitem],(int)StatusPet.Hunger*100/StatusPet.MaxHunger);
+    SetMenuItemIconArray(data,item,M_ICONS+9);
+    SetMenuItemText(data, item, ws, curitem);
+    break;  
+  case 3:
+    ws=AllocMenuWS(data,strlen(smenutexts[curitem])+5);
+    wsprintf(ws,"%t:%d%%",smenutexts[curitem],(int)StatusPet.Happiness*100/StatusPet.MaxHappiness);
+    SetMenuItemIconArray(data,item,M_ICONS+10);
+    SetMenuItemText(data, item, ws, curitem);
+    break;  
+  case 4:
+    ws=AllocMenuWS(data,strlen(smenutexts[curitem])+5);
+    wsprintf(ws,"%t:%d%%",smenutexts[curitem],100-(int)StatusPet.Dirtiness*100/StatusPet.MaxDirtiness);
+    SetMenuItemIconArray(data,item,M_ICONS+11);
+    SetMenuItemText(data, item, ws, curitem);
+    break;  
+  case 5:
+    ws=AllocMenuWS(data,strlen(smenutexts[curitem])+5);
+    wsprintf(ws,"%t:%d%%",smenutexts[curitem],(int)StatusPet.Boredom*100/StatusPet.MaxBoredom);
+    SetMenuItemIconArray(data,item,M_ICONS+12);
+    SetMenuItemText(data, item, ws, curitem);
+    break;  
+  case 6:
+    ws=AllocMenuWS(data,strlen(smenutexts[curitem])+5);
+    wsprintf(ws,"%t:%d%%",smenutexts[curitem],(int)StatusPet.Behaviour*100/StatusPet.MaxBehaviour);
+    SetMenuItemIconArray(data,item,M_ICONS+13);
+    SetMenuItemText(data, item, ws, curitem);
+    break;  
+  }
+}
+
+static const MENU_DESC smenu=
+{
+  8,smenu_keyhook,smenu_ghook,NULL,
+  smenusoftkeys,
+  &smenu_skt,
+  0x11,//MENU_FLAG,
+  smenuitemhandler,
+  NULL,//menuitems,
+  NULL,//menuprocs,
+  0  //count items
+};
+
+void ShowStatus(void)
+{
+  patch_header(&smenuhdr);
+  StatusMenu_ID=CreateMenu(0,0,&smenu,&smenuhdr,0,7,0,0);
+  
+}
