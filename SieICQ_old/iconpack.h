d7411a9309ee13bb4cc4e78d16b3ae8da77df084
anis_1981@mail.ru <anis_1981@mail.ru@f75f4211-f83f-0410-8a70-e9417351f98b>
2008-11-21 12:52:56 +0000

    начал работу над графикой еще не доделал - не компилится

---
+++ SieICQ_old/iconpack.h
@@ -0,0 +1,24 @@
+#ifndef _ICONPACK_H_
+#define _ICONPACK_H_
+
+enum ICON_ID
+{
+  IMG_OFFLINE,
+  IMG_ONLINE,
+  IMG_AWAY,
+  IMG_DND,
+  IMG_NA,
+  IMG_OCCUPIED,
+  IMG_FFC,
+  IMG_INVISIBLE,
+  TOTAL_ICONS_NUM
+};
+
+
+
+void LoadIcon();
+void FreeIcon();
+  
+int IconPack[TOTAL_ICONS_NUM];
+
+#endif
